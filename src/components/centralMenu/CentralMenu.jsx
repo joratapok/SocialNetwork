a7f4781fe23dec587336d3cfeaad7393b7ca7fe5
joratapok <joratapok@gmail.com>
2021-03-01 16:52:10 +0800

    work with design left column

--- src/components/centralMenu/CentralMenu.jsx
+++ src/components/centralMenu/CentralMenu.jsx
@@ -4,18 +4,21 @@ import classes from './CentralMenu.module.css'
 const CentralMenu = () => {
     return(
         <div className={classes.wrapper}>
-            <div className={classes.centralMenu}>
-                <div className={classes.item}>
-                    About
-                </div>
-                <div className={classes.item}>
-                    Photos
-                </div>
-                <div className={classes.item}>
-                    Friends
-                </div>
-                <div className={classes.item}>
-                    More
+            <div className={classes.wrapperCentral}>
+                <div className={classes.leftCentralMenu}></div>
+                <div className={classes.rightCentralMenu}>
+                    <div className={classes.item}>
+                        About
+                    </div>
+                    <div className={classes.item}>
+                        Photos
+                    </div>
+                    <div className={classes.item}>
+                        Friends
+                    </div>
+                    <div className={classes.item}>
+                        More
+                    </div>
                 </div>
             </div>
         </div>
